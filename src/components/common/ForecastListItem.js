9a637d8ed6bd4523d844bfc3619aa47c17ad7661
Thiago Scolari <thiagoscolari@MacBook-Pro-de-Thiago.local>
2019-01-19 18:32:47 +0100

    commit

--- src/components/common/ForecastListItem.js
+++ src/components/common/ForecastListItem.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { } from '../../actions/location_actions'
 import moment from 'moment'
 import {
     TiWeatherCloudy,
